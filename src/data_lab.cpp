d2b87ccdedc13b784e6ad5b156c5ba69a7b51c34
Zhengqi Dai <124090101@link.cuhk.edu.cn>
2026-01-21 23:56:54 +0800

    用helper函数统一起来

--- src/data_lab.cpp
+++ src/data_lab.cpp
@@ -2,35 +2,29 @@
 #include <cstdint>
 
 namespace data_lab {
-
-int32_t add(int32_t a, int32_t b) {
-    uint32_t ua = static_cast<uint32_t>(a);
-    uint32_t ub = static_cast<uint32_t>(b);
-    bool carry = 0;
+uint32_t helper_add(uint32_t a, uint32_t b, bool carry = 0){
     uint32_t sum = 0;
     for(int i = 0;i<32;i++){
-        bool ai = (ua & (1 << i)) != 0;
-        bool bi = (ub & (1 << i)) != 0;
+        bool ai = (a & (1 << i)) != 0;
+        bool bi = (b & (1 << i)) != 0;
         bool si = ai ^ bi ^ carry; // sum bit at position i
         sum |= static_cast<uint32_t>(si) << i; // set the i-th bit of sum
         carry = (ai & bi) | (carry & (ai ^ bi)); // update carry
-    }  
-    return static_cast<int32_t>(sum);
+    } 
+    return sum;
+}
+int32_t add(int32_t a, int32_t b) {
+    uint32_t ua = static_cast<uint32_t>(a);
+    uint32_t ub = static_cast<uint32_t>(b);
+
+    return static_cast<int32_t>(helper_add(ua, ub));
 }
 
 int32_t subtract(int32_t a, int32_t b) {
     uint32_t ua = static_cast<uint32_t>(a);
     uint32_t ub = static_cast<uint32_t>(~b);
-    bool carry = 1;
-    uint32_t sum = 0;
-    for(int i = 0;i<32;i++){
-        bool ai = (ua & (1 << i)) != 0;
-        bool bi = (ub & (1 << i)) != 0;
-        bool si = ai ^ bi ^ carry; // sum bit at position i
-        sum |= static_cast<uint32_t>(si) << i; // set the i-th bit of sum
-        carry = (ai & bi) | (carry & (ai ^ bi)); // update carry
-    }  
-    return static_cast<int32_t>(sum);
+    
+    return static_cast<int32_t>(helper_add(ua, ub, 1));
 }
 
 int32_t multiply(int32_t a, int32_t b) {
@@ -38,7 +32,7 @@ int32_t multiply(int32_t a, int32_t b) {
     uint32_t ub = static_cast<uint32_t>(b);
     uint64_t ans = 0;
     for(int i = 0;i<32;i++){
-
+        if((ub & (1 << i)) != 0) add(ua, )
     }   
 }
 
