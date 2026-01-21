82051f29d886aef47f7e36a51e389e7bb9af67f4
Zhengqi Dai <124090101@link.cuhk.edu.cn>
2026-01-21 19:27:32 +0800

    add初步实现

--- src/data_lab.cpp
+++ src/data_lab.cpp
@@ -4,7 +4,18 @@
 namespace data_lab {
 
 int32_t add(int32_t a, int32_t b) {
-    return a + b;
+    uint32_t ua = static_cast<uint32_t>(a);
+    uint32_t ub = static_cast<uint32_t>(b);
+    bool carry = 0;
+    uint32_t sum = 0;
+    for(int i = 0;i<32;i++){
+        bool ai = (ua & (1 << i)) != 0;
+        bool bi = (ub & (1 << i)) != 0;
+        bool si = ai ^ bi ^ carry; // sum bit at position i
+        sum += static_cast<uint32_t>(si) << i; // set the i-th bit of sum
+        carry = (ai & bi) | (carry & (ai ^ bi)); // update carry
+    }  
+    return static_cast<int32_t>(sum);
 }
 
 int32_t subtract(int32_t a, int32_t b) {
